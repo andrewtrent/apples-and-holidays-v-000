e20721b6aaca67ba6dbd8c807df250f1aedba151
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-25 19:10:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supplies|
-      str = (day.to_s.tr!("_", " ")).gsub(/\w+/, &:capitalize)
+      str = day.to_s.tr!("_", " ")
+      str.gsub(/\w+/, &:capitalize)
       str << supplies.join(", ")
       puts str
     end
